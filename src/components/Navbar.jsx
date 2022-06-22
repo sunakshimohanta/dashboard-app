92f4d248bb3c1e88c2ed279a638803c6eee2cc05
Sunakshi Mohanta <er.sunakshimohanta@gmail.com>
2022-06-22 16:22:19 +0530

    Second Commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,9 +21,9 @@ dotColor }) => (
      p-3 hover:bg-light-gray">
        <span style={{background: dotColor}}
        className="absolute inline-flex 
-       rounded-full h-2 w-2 right-2 top-2">
+       rounded-full h-2 w-2 right-2 top-2"/>
         {icon}
-       </span>
+      
      </button>
    </TooltipComponent>
 );
